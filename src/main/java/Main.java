e448323d177d5277091d7179556f58f8abf8f906
aleneagu96 <aleneagu96@gmail.com>
2020-10-18 13:16:46 +0300

    add new sout for switch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Radu");
+        System.out.println("Switch to main branch again");
     }
 }
