7ac108d1745d721757b7c6466d2e697d57c8ee55
aleneagu96 <aleneagu96@gmail.com>
2020-10-24 09:45:01 +0300

    Add new line to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Radu");
         System.out.println("Switch to main branch again");
+        System.out.println("Add a new line in main");
     }
 }
