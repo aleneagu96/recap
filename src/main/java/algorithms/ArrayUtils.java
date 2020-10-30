3ed9f577cfeea11629d736a203294e55f3d8da95
aleneagu96 <aleneagu96@gmail.com>
2020-10-30 15:49:28 +0200

    Add array and String exercises

--- src/main/java/algorithms/ArrayUtils.java
+++ src/main/java/algorithms/ArrayUtils.java
@@ -1,5 +1,9 @@
 package algorithms;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class ArrayUtils {
 
     //    check if array is palindrome
@@ -18,4 +22,119 @@ public class ArrayUtils {
         }
         return isPalindrome;
     }
+
+    public static boolean isPalindromen(int[] array) {    // 1 !=
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != array[array.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+//    Scrieti un program care sa inverseze elementele intr-un array de tip int (array dat ca parametru)
+//    Ex: [1, 34, 89, 2, 7]  - inversare array => [7, 2, 89, 34, 1]
+
+    // i-0 = 1-> 7
+    // i-1 = 34-> 2
+    // i-2 = 89-> 89
+    // i-3 = 2-> 34
+    // i-4 = 7-> 1
+
+    public static List<Integer> inverse(Integer[] myArray) {
+        List<Integer> list = Arrays.asList(myArray);
+        Collections.reverse(list);
+        return list;
+    }
+
+
+//    Scrieti un program care sorteaza un array de elemente int (array dat ca parametru)
+//Ex: buuble sort, selection sort, [2, 9, 3, 10] - sortare array => [2,3, 9, 10]
+
+    public static List<Integer> sort(Integer[] myArray) {
+        for (int i = 0; i < myArray.length; i++) {
+            List<Integer> arrayToList = Arrays.asList(myArray);
+            Collections.sort(arrayToList);
+        }
+        return Arrays.asList(myArray);
+    }
+
+    public static String sortTheArray(int[] array) {
+        int t = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < (array.length - i); j++) {
+                if (array[j - 1] > array[j]) {
+                    t = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = t;
+                }
+            }
+
+        }
+        return Arrays.toString(array);
+    }
+
+//    Scrieti un program care verifica daca un numar e prim. (nr dat ca parametru)
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+//    Scrieti un program care elemina dintr-un array elementele duplicate
+//    Ex: [2, 4, 7, 2, 8, 2] - elimina duplicate => [2,4,7,8]
+
+//    public static List removeDuplicates(Integer[] array) {
+//        List<Integer> arrayList = Arrays.asList(array);
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = i + 1; j < array.length; j++) {
+//                if (array[i] == array[j]) {
+//                    arrayList.remove(arrayList.indexOf(i)); // UnsupportedOperationException
+//                }
+//            }
+//        }
+//        return arrayList;
+//    }
+
+//    Scrieti un program care determina numarul de aparitii unei litere date ca parametru
+
+    public static int aparitions(String word, String x) {
+        int apparition = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.substring(i, i + 1).matches(x)) {
+                apparition++;
+            }
+        }
+        return apparition;
+    }
+
+//    Scrieti un program care determina numarul de vocale dintr-un string ( => array de caractere). (String-ul dat ca parametru)
+//Ex: “afara ploua” => 6 vocale (a, a, a, o, u, a)
+
+    public static int vowelCounter(String words) {
+        int numberOfVowels = 0;
+        for (int i = 0; i < words.length(); i++) {
+            if (words.charAt(i) == 'a' || words.charAt(i) == 'e' || words.charAt(i) == 'i' || words.charAt(i) == 'o' || words.charAt(i) == 'u') {
+                numberOfVowels++;
+            }
+        }
+        return numberOfVowels;
+    }
+
+//    Scrieti un program care elimina un caracter (dat ca parametru) dintr-un string.
+
+    public static StringBuffer eliminateCharacter(StringBuffer word, int index) {
+        StringBuffer resultString = word.deleteCharAt(index);
+        return resultString;
+    }
+
+
 }
+
